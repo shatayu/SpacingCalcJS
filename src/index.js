771d757aa7526e9895d288070ec24f1d31afb619
shatayu <shatayu@outlook.com>
2018-06-30 18:24:48 +0530

    removed buttons, working on dynamically inserting input into function

--- src/index.js
+++ src/index.js
@@ -10,30 +10,31 @@ function checkAll() {
     }
 }
 
-function uncheckAll() {
-    let checkboxes = document.getElementsByClassName("spacerOption");
+// function uncheckAll() {
+//     let checkboxes = document.getElementsByClassName("spacerOption");
 
-    for (let i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].checked = false;
-    }
-}
+//     for (let i = 0; i < checkboxes.length; i++) {
+//         checkboxes[i].checked = false;
+//     }
+// }
 
 checkAll();
 
-document.getElementById("checkall").addEventListener("click", checkAll);
-
-document.getElementById("uncheckall").addEventListener("click", uncheckAll);
-
 // handle input submission
-document.getElementById("submit").addEventListener("click", () => {
+document.getElementById("text").addEventListener("keyup", () => {
     document.getElementById("response").innerHTML = "";
     let input = document.getElementById("text").value;
-    let availableSpacers = spacers.getSpacers(); 
-    let result = calc.calcSpacers(Math.round(1000 * parseFloat(input)), availableSpacers);
+    let number = parseFloat(input.trim());
+    let availableSpacers = spacers.getSpacers();
 
-    for (let i = 0; i < result.length; i++) {
-        if (result[i].quantity > 0) {
-            document.getElementById("response").appendChild(spacers.displaySpacer(result[i]));
+    let result;
+    if (input.trim().length > 0 && number !== NaN) {
+        result = calc.calcSpacers(Math.round(1000 * number), availableSpacers);
+
+        for (let i = 0; i < result.length; i++) {
+            if (result[i].quantity > 0) {
+                document.getElementById("response").appendChild(spacers.displaySpacer(result[i]));
+            }
         }
     }
 })
