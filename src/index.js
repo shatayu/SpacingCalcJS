516b2df6aa232359ff7ce8c7524bc034da3e4540
shatayu <shatayu@outlook.com>
2018-06-30 17:54:11 +0530

    worked on design

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 let spacers = require("./spacers");
 let calc = require("./calc");
 
+// manage checking and unchecking all
+function checkAll() {
+    let checkboxes = document.getElementsByClassName("spacerOption");
+
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = true;
+    }
+}
+
+function uncheckAll() {
+    let checkboxes = document.getElementsByClassName("spacerOption");
+
+    for (let i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = false;
+    }
+}
+
+checkAll();
+
+document.getElementById("checkall").addEventListener("click", checkAll);
+
+document.getElementById("uncheckall").addEventListener("click", uncheckAll);
+
+// handle input submission
 document.getElementById("submit").addEventListener("click", () => {
+    document.getElementById("response").innerHTML = "";
     let input = document.getElementById("text").value;
     let availableSpacers = spacers.getSpacers(); 
     let result = calc.calcSpacers(Math.round(1000 * parseFloat(input)), availableSpacers);
@@ -10,21 +35,5 @@ document.getElementById("submit").addEventListener("click", () => {
         if (result[i].quantity > 0) {
             document.getElementById("response").appendChild(spacers.displaySpacer(result[i]));
         }
-    }
-})
-
-document.getElementById("checkall").addEventListener("click", () => {
-    let checkboxes = document.getElementsByClassName("spacerOption");
-
-    for (let i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].checked = true;
-    }
-})
-
-document.getElementById("uncheckall").addEventListener("click", () => {
-    let checkboxes = document.getElementsByClassName("spacerOption");
-
-    for (let i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].checked = false;
     }
 })
